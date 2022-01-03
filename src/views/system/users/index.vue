af87e78cd3006ccff8fccb601a2dbd735249d23a
陶云杰 <taoyj@taoyunjiedeMacBook-Pro.local>
2022-01-03 19:28:56 +0800

    feat(users/index.vue): 根据左树id查询表数据
    re #5

--- src/views/system/users/index.vue
+++ src/views/system/users/index.vue
@@ -1,17 +1,21 @@
 <template>
   <el-container>
     <el-aside>
-      <el-form size="mini" :inline="true" :model="formInline" class="tree-area">
+      <el-form :inline="true" :model="formInline" class="tree-area">
         <el-form-item>
-          <el-input v-model="formInline.user" placeholder="公司/部门" />
+          <!-- <el-input v-model="formInline.user" placeholder="公司/部门" /> -->
+          <el-input
+            v-model="filterText"
+            placeholder="输入公司/部门"
+          />
         </el-form-item>
-        <el-form-item>
+        <!-- <el-form-item>
           <el-button type="primary" @click="onSubmit">查询</el-button>
-        </el-form-item>
+        </el-form-item> -->
       </el-form>
-      <el-tree :data="treeData" :expand-on-click-node="false" :props="defaultProps" @node-click="handleNodeClick" />
+      <el-tree ref="tree" :filter-node-method="filterNode" :data="treeData" :expand-on-click-node="false" :props="defaultProps" @node-click="handleNodeClick" />
     </el-aside>
-    <table-area :columns="columns" :title="title" url="/vue-element-admin/users/list" />
+    <table-area :columns="columns" :title="title" :url="`/vue-element-admin/users/list/${1}`" />
   </el-container>
 </template>
 
@@ -24,6 +28,7 @@ export default {
   },
   data() {
     return {
+      filterText: '',
       formInline: {
         user: '',
         region: ''
@@ -58,36 +63,48 @@ export default {
       ],
       tableData: [],
       treeData: [{
+        id: 1,
         label: '一级 1',
         children: [{
+          id: 2,
           label: '二级 1-1',
           children: [{
+            id: 3,
             label: '三级 1-1-1'
           }]
         }]
       }, {
+        id: 11,
         label: '一级 2',
         children: [{
+          id: 12,
           label: '二级 2-1',
           children: [{
+            id: 13,
             label: '三级 2-1-1'
           }]
         }, {
+          id: 14,
           label: '二级 2-2',
           children: [{
             label: '三级 2-2-1'
           }]
         }]
       }, {
+        id: 21,
         label: '一级 3',
         children: [{
+          id: 22,
           label: '二级 3-1',
           children: [{
+            id: 23,
             label: '三级 3-1-1'
           }]
         }, {
+          id: 31,
           label: '二级 3-2',
           children: [{
+            id: 32,
             label: '三级 3-2-1'
           }]
         }]
@@ -98,9 +115,18 @@ export default {
       }
     }
   },
+  watch: {
+    filterText(val) {
+      this.$refs.tree.filter(val)
+    }
+  },
   methods: {
+    filterNode(value, data) {
+      if (!value) return true
+      return data.label.indexOf(value) !== -1
+    },
     handleNodeClick(data) {
-      console.log(data)
+      console.log(data['id'])
     },
     onSubmit() {
 
