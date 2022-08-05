fdf1bc612c0d4ed66379de813af58f52a97267e1
kushu001 <yj_tao123@sina.com>
2022-08-05 18:41:53 +0800

    perf(user/index.vue): 身份号不显示及电话号码不能保存的问题处理

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -56,6 +56,9 @@ export default {
             width: 200,
             searchConfig: {
               hidden: true
+            },
+            formConfig: {
+              hidden: true
             }
           },
           {
