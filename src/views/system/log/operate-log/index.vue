5e1d3ef9661c25696f90e2bcafcb0ff61fed2d6a
kushu001 <yj_tao123@sina.com>
2022-08-15 22:13:43 +0800

    feat(operate-log/index.vue): 增加操作日志页面

--- src/views/system/log/operate-log/index.vue
+++ src/views/system/log/operate-log/index.vue
@@ -1,3 +1,51 @@
 <template>
-  <div>操作日志</div>
+  <cdp-table :table-config="tableConfig" />
 </template>
+<script>
+import CdpTable from '@/components/cdp-ui/cdp-template/cdp-table'
+
+export default {
+  components: {
+    CdpTable
+  },
+  data() {
+    return {
+      tableConfig: {
+        url: '/api/v1/operate-log',
+        columns: [
+          {
+            name: 'user',
+            label: '操作人'
+          },
+          {
+            name: 'url',
+            label: '地址',
+            searchConfig: {
+              hidden: true
+            }
+          },
+          {
+            name: 'request_method',
+            label: '请求方法'
+          },
+          {
+            name: 'resource_name',
+            label: '资源名称'
+          },
+          {
+            name: 'payload',
+            label: '载荷',
+            searchConfig: {
+              hidden: true
+            }
+          },
+          {
+            name: 'create_time',
+            label: '操作时间'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
