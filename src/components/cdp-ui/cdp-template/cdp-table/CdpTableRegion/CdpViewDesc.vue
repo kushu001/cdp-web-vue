6cff3750d8e91cf7ef3ba54e62995066d8c69472
kushu001 <yj_tao123@sina.com>
2022-08-29 21:06:33 +0800

    style(cdpviewdesc): 去掉无用代码

--- src/components/cdp-ui/cdp-template/cdp-table/CdpTableRegion/CdpViewDesc.vue
+++ src/components/cdp-ui/cdp-template/cdp-table/CdpTableRegion/CdpViewDesc.vue
@@ -13,41 +13,6 @@
         </template>
         {{ form[item.name] }}
       </el-descriptions-item>
-      <!-- <el-descriptions-item>
-        <template slot="label">
-          <i class="el-icon-user" />
-          用户名
-        </template>
-        kooriookami
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template slot="label">
-          <i class="el-icon-mobile-phone" />
-          手机号
-        </template>
-        18100000000
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template slot="label">
-          <i class="el-icon-location-outline" />
-          居住地
-        </template>
-        苏州市
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template slot="label">
-          <i class="el-icon-tickets" />
-          备注
-        </template>
-        <el-tag size="small">学校</el-tag>
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template slot="label">
-          <i class="el-icon-office-building" />
-          联系地址
-        </template>
-        江苏省苏州市吴中区吴中大道 1188 号
-      </el-descriptions-item> -->
     </el-descriptions>
   </el-drawer>
 </template>
