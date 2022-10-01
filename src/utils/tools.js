64888024cb6411a4ed425f8fe1ed439002e8ccdc
kushu001 <yj_tao123@sina.com>
2022-10-01 17:14:57 +0800

    fix(utils/tools.js): 构建树，在不知道根节点id的情况下，构建树可能失败的问题处理

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,36 +1,26 @@
 import Layout from '@/layout'
 
 export function buildTree({ data, id = 'id', pid = 'pid', children = 'children' }) {
-  // 深拷贝一份数据
-  var copy = JSON.parse(JSON.stringify(data))
-  // 创建一个存储数据的对象
-  var obj = {}
-  // 遍历数据，将数据所有数据添加到对象中，key为数据id，value为原数据对象
-  copy.forEach((item, index) => {
-    obj[item[id]] = item
+  // * 先生成parent建立父子关系
+  const obj = {}
+  data.forEach((item) => {
+    obj[item.id] = item
   })
-  // 创建一个最终返回的数组
-  var res = []
-  // 遍历数据开始处理
-  copy.forEach(item => {
-    // 将root数据添加进res数组， 因为数据使引用类型，子元素数据都会带过来，下面的循环会处理子元素数据
-    if (item[pid] === 0) {
-      res.push(item)
-    }
-    // 梳理子元素数据
-    for (var key in obj) {
-      // 当一层元素id 和 二层元素pId一致，那么，二层遍历的元素就是一层遍历元素的children
-      if (item[id] === obj[key][pid]) {
-        // 处理数据children
-        if (item[children]) {
-          item[children].push(obj[key])
-        } else {
-          item[children] = [obj[key]]
-        }
-      }
+  // * obj -> {1001: {id: 1001, parentId: 0, name: 'AA'}, 1002: {...}}
+  // console.log(obj, "obj")
+  const parentList = []
+  data.forEach((item) => {
+    const parent = obj[item[pid]]
+    if (parent) {
+      // * 当前项有父节点
+      parent[children] = parent[children] || []
+      parent[children].push(item)
+    } else {
+      // * 当前项没有父节点 -> 顶层
+      parentList.push(item)
     }
   })
-  return res
+  return parentList
 }
 
 /**
