f91a7b053e806092e0da9a0d3135d0b2d08b48d4
kushu001 <yj_tao123@sina.com>
2022-10-24 16:59:11 +0800

    feat(resource/index.vue): 新增资源管理模块

---
+++ src/views/system/resource/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <cdp-table :table-config="tableConfig" />
+</template>
+<script>
+import CdpTable from '@/components/cdp-ui/cdp-template/cdp-table'
+
+export default {
+  components: {
+    CdpTable
+  },
+  data() {
+    return {
+      tableConfig: {
+        title: '资源信息',
+        url: '/api/v1/resource',
+        columns: [
+          {
+            name: 'name',
+            label: '资源名称',
+            formConfig: {
+              rules: [{ required: true, message: '请输入资源名称', trigger: 'blur' }]
+            }
+          },
+          {
+            name: 'sign',
+            label: '标识',
+            formConfig: {
+              rules: [{ required: true, message: '请输入标识', trigger: 'blur' }]
+            }
+          },
+          {
+            name: 'url',
+            label: 'URL',
+            formConfig: {
+              rules: [{ required: true, message: '请输入URL', trigger: 'blur' }]
+            }
+          },
+          {
+            name: 'create_time',
+            label: '创建日期',
+            formConfig: {
+              hidden: true
+            },
+            searchConfig: {
+              hidden: true
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
--- src/views/system/resources/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>资源管理-开发中...</div>
-</template>
