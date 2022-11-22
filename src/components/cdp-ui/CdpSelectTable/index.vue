44b24944cdc56df5f08a6e0df8d7d96c2a5b379e
kushu001 <yj_tao123@sina.com>
2022-11-22 16:23:22 +0800

    feat(cdpselecttable/index.vue): 允许清空组件值

--- src/components/cdp-ui/CdpSelectTable/index.vue
+++ src/components/cdp-ui/CdpSelectTable/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <el-input v-model="val" placeholder="请选择" class="input-with-select" :readonly="true">
-      <el-button slot="append" icon="el-icon-s-grid" @click="dialogTableVisible = true" />
+      <el-button slot="prepend" icon="el-icon-refresh-left" title="重置" @click="reset()" />
+      <el-button slot="append" icon="el-icon-search" @click="dialogTableVisible = true" />
     </el-input>
     <el-dialog title="选择人员" :fullscreen="true" width="1000px" :visible.sync="dialogTableVisible" :append-to-body="true" :before-close="handleClose">
       <el-card>
         <cdp-search-region ref="search" :conditions="columns" :search="searchHandler" />
-        <cdp-table-region ref="table" v-slot:default="slotProps" title="账号" :columns="columns" :selection="false" :url="url" :operations="[]">
+        <cdp-table-region ref="table" v-slot:default="slotProps" :columns="columns" :selection="false" :url="url" :operations="[]">
           <el-button type="primary" style="margin-left:10px" size="small" @click="confirm(slotProps.scope)">
             确 认
           </el-button>
@@ -68,6 +69,10 @@ export default {
       this.val = row.name
       this.dialogTableVisible = false
       this.$emit('input', row.id)
+    },
+    reset() {
+      this.val = null
+      this.$emit('input', null)
     },
     handleClose() {
       this.$refs.search.resetHandler()
