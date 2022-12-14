baf42435e71ef4181c663c4e0c464485404c5e36
kushu001 <yj_tao123@sina.com>
2022-12-14 23:05:00 +0800

    fix(profile/index.vue): 解决简介页面打不开的bug

--- src/router/index.js
+++ src/router/index.js
@@ -76,7 +76,21 @@ export const constantRoutes = [
         meta: { title: '仪表盘', icon: 'dashboard', affix: true }
       }
     ]
-  }//,
+  },
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
+  }
   // {
   //   path: '/system',
   //   component: Layout,
