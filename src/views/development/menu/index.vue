c8cedee58c907e91613c0e851432540bef0a3371
kushu001 <yj_tao123@sina.com>
2022-12-14 23:16:04 +0800

    perf(menu/index.vue): 将完整的菜单模块调整至开发者选项目录下

--- src/views/development/menu/index.vue
+++ src/views/development/menu/index.vue
@@ -2,40 +2,78 @@
   <el-container>
     <el-aside>
       <el-card>
-        <cdp-search-tree placeholder="输入关键字进行过滤" :url="`${url}?v=${v}`" :props="{label:'title'}" :click-hanlder="clickHanlder" />
+        <cdp-search-tree placeholder="输入关键字进行过滤" :url="`${url}?v=${v}`" :default-expanded-keys="defaultKeys" :props="{label:'title'}" :accordion="true" :click-hanlder="clickHanlder" />
       </el-card>
     </el-aside>
     <el-main>
       <el-card>
-        <el-form ref="form" :inline="true" :model="form" label-width="80px" style="padding-left:50px">
-          <el-form-item label="菜单名称">
-            <el-input v-model="form.title" style="width:300px" />
-          </el-form-item>
-          <el-form-item label="属性名称">
-            <el-input v-model="form.name" style="width:300px" />
-          </el-form-item>
-          <el-form-item label="URL">
-            <el-input v-model="form.url" style="width:300px" />
-          </el-form-item>
-          <el-form-item label="类型">
-            <el-radio-group v-model="form.type" style="width:300px">
-              <el-radio label="0">菜单</el-radio>
-              <el-radio label="1">操作</el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="图标">
-            <cdp-select-icon v-model="form.icon" style="width:300px" />
-          </el-form-item>
-          <el-form-item label="排序">
-            <el-input-number v-model="form.sort" style="width:300px" />
-          </el-form-item>
-          <el-form-item label="备注">
-            <el-input v-model="form.remark" type="textarea" style="width:690px" />
-          </el-form-item>
+        <div slot="header">
+          <span>{{ (!isNew)? (form.title? form.title: '菜单信息'):parentTitle===''? (form.title? form.title: '菜单信息') : parentTitle+'-'+(form.title?form.title:'') }}</span>
+          <el-button v-if="!isNew && form.type === '0'" style="float: right;" size="mini" type="success" plain @click="resetSubFormHandler">新建子级</el-button>
+          <el-button v-if="!isNew" style="float: right;margin-right: 5px" size="mini" type="success" @click="resetFormHandler">新建</el-button>
+        </div>
+        <el-form ref="form" :model="form" label-width="80px" style="padding-left:50px">
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="名称" prop="title">
+                <el-input v-model="form.title" style="width:100%" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="URL" prop="url">
+                <el-input v-model="form.url" style="width:100%" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="类型" prop="type">
+                <el-radio-group v-model="form.type" style="width:100%">
+                  <el-radio :disabled="isNew && form.type === '1'" label="0">菜单</el-radio>
+                  <el-radio label="1">操作</el-radio>
+                  <el-radio label="2">接口</el-radio>
+                  <!-- <el-radio v-if="Object.keys(data).length > 0" label="1">操作</el-radio> -->
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="操作标识" prop="permission">
+                <el-input v-model="form.permission" style="width:100%" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="是否启用" prop="enabled">
+                <el-switch v-model="form.enabled" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="图标" prop="icon">
+                <!-- <el-input v-model="form.icon" style="width:300px" /> -->
+                <cdp-select-icon v-model="form.icon" style="width:100%" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="排序" prop="sort">
+                <el-input-number v-model="form.sort" style="width:100%" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="备注" prop="remark">
+                <el-input v-model="form.remark" type="textarea" style="width:100%" />
+              </el-form-item>
+            </el-col>
+          </el-row>
           <el-row type="flex" justify="center">
             <el-form-item>
-              <el-button v-if="type !== '1' || !form.id" type="primary" @click="createMenuHandler">新 建</el-button>
-              <el-button v-if="form.id" type="primary" @click="updateMenuHandler">修 改</el-button>
+              <el-button v-if="isNew" type="primary" @click="createMenuHandler">新 建</el-button>
+              <el-button v-if="form.id" type="primary" @click="updateMenuHandler">更 新</el-button>
               <el-button v-if="form.id" type="primary" plain @click="deleteMenuHandler(form.id)">删 除</el-button>
             </el-form-item>
           </el-row>
@@ -47,6 +85,7 @@
 <script>
 import { add, update, deleteById } from '@/api/api'
 import CdpSelectIcon from '@/components/cdp-ui/CdpSelectIcon'
+
 export default {
   components: {
     CdpSelectIcon,
@@ -54,23 +93,34 @@ export default {
   },
   data() {
     return {
-      form: { sort: 1 },
+      form: { pid: 0, sort: 1, type: '0' },
       url: '/api/v1/menu',
       v: new Date().getTime(),
+      // node: {},
       data: {},
-      type: '1'
+      defaultKeys: [],
+      isNew: true,
+      parentTitle: ''
     }
   },
   methods: {
     createMenuHandler() {
       this.$refs['form'].validate((valid) => {
         if (valid) {
-          this.form.pid = this.data.id ? this.data.id : this.form.pid
           add(this.form, this.url).then(response => {
             this.v = new Date().getTime()
+            this.defaultKeys = [this.form.pid]
+            this.$message({
+              type: 'success',
+              message: '新建成功'
+            })
           })
         } else {
           console.log('error submit!!')
+          this.$message({
+            type: 'danger',
+            message: '新建失败'
+          })
           return false
         }
       })
@@ -80,9 +130,18 @@ export default {
         if (valid) {
           update(this.form, this.url).then(response => {
             this.v = new Date().getTime()
+            this.defaultKeys = [this.form.id]
+            this.$message({
+              type: 'success',
+              message: '更新成功'
+            })
           })
         } else {
           console.log('error submit!!')
+          this.$message({
+            type: 'danger',
+            message: '更新失败'
+          })
           return false
         }
       })
@@ -97,6 +156,10 @@ export default {
           deleteById(`${this.url}/${id}`).then(({ data }) => {
             if (data.code === 200) {
               this.v = new Date().getTime()
+              this.defaultKeys = [this.form.pid]
+              this.$refs.form.resetFields()
+              this.form = { ...this.form, id: undefined, type: '0' }
+              this.isNew = true
               this.$message({
                 type: 'success',
                 message: '删除成功!'
@@ -116,10 +179,28 @@ export default {
         })
       }
     },
-    clickHanlder(data, node) {
+    clickHanlder(data, node, isClicked) {
+      // console.log(data)
       this.form = { ...data }
-      this.data = data
-      this.type = data.type
+      this.isNew = !isClicked
+      this.parentTitle = ''
+    },
+    resetFormHandler() {
+      const type = this.form.type
+      // console.log(type)
+      this.$refs.form.resetFields()
+      this.form = { ...this.form, id: undefined, type: type }
+      this.isNew = true
+      // console.log(this.form)
+    },
+    resetSubFormHandler() {
+      this.parentTitle = this.form.title
+      const type = this.form.type
+      // console.log(type)
+      this.$refs.form.resetFields()
+      this.form = { ...this.form, id: undefined, pid: this.form.id, type: type }
+      this.isNew = true
+      // console.log(this.form)
     }
   }
 }
