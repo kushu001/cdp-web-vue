6ab09456b17899fc1269927e2cf3c2c624e199c3
kushu001 <yj_tao123@sina.com>
2023-07-04 16:38:25 +0800

    feat(role/authorization): 增加接口权限配置功能

--- src/views/system/role/components/Authorization/index.vue
+++ src/views/system/role/components/Authorization/index.vue
@@ -1,8 +1,8 @@
 <template>
   <span>
     <el-button type="text" size="small" @click="authorizedDialog">授 权</el-button>
-    <el-drawer title="授权" :visible.sync="drawer" :append-to-body="true">
-      <el-tabs tab-position="left">
+    <el-drawer title="授权" :visible.sync="drawer" :size="size" :append-to-body="true">
+      <el-tabs tab-position="left" @tab-click="handlerTabs">
         <el-tab-pane label="菜单权限" style="margin-right:10px">
           <cdp-search-tree :key="keys.menuKey" v-model="menus" placeholder="输入关键字进行过滤" :props="{label:'title'}" url="/api/v1/menu/type/0" :show-checkbox="true" />
           <!-- <div class="footer">
@@ -19,12 +19,14 @@
             </el-row>
           </div> -->
         </el-tab-pane>
-        <el-tab-pane label="接口权限">开发中...</el-tab-pane>
+        <el-tab-pane label="接口权限">
+          <cdp-table v-if="index==='2'" v-model="resources" :table-config="tableConfig" />
+        </el-tab-pane>
         <el-tab-pane label="数据权限">开发中...</el-tab-pane>
       </el-tabs>
       <div class="footer">
-        <el-row :gutter="20">
-          <el-col><el-button type="primary" @click="authorizedOperations">保存</el-button></el-col>
+        <el-row type="flex" justify="center">
+          <el-button type="primary" @click="hanlderSave">保存</el-button>
         </el-row>
       </div>
     </el-drawer>
@@ -32,10 +34,12 @@
 </template>
 <script>
 import CdpSearchTree from '@/components/cdp-ui/CdpSearchTree'
-import { authorizedMenus, authorizedOperations, menus, operations } from '@/api/role'
+import CdpTable from '@/components/cdp-ui/cdp-template/cdp-table'
+import { authorizedMenus, authorizedOperations, authorizedResources, menus, operations, resources } from '@/api/role'
 export default {
   components: {
-    CdpSearchTree
+    CdpSearchTree,
+    CdpTable
   },
   props: {
     row: {
@@ -46,12 +50,74 @@ export default {
   data() {
     return {
       drawer: false,
+      size: '25%',
       menus: [],
       operations: [],
-      keys: { menuKey: 1, operationKey: 1 }
+      resources: [],
+      index: '0',
+      keys: { menuKey: 1, operationKey: 1 },
+      tableConfig: {
+        title: '资源信息',
+        url: '/api/v1/resource',
+        flippingMemory: true,
+        hOpn: {
+          default: false
+        },
+        rOpn: {
+          default: false
+        },
+        columns: [
+          {
+            name: 'name',
+            label: '资源名称',
+            formConfig: {
+              rules: [{ required: true, message: '请输入资源名称', trigger: 'blur' }]
+            }
+          },
+          {
+            name: 'sign',
+            label: '标识',
+            formConfig: {
+              rules: [{ required: true, message: '请输入标识', trigger: 'blur' }]
+            }
+          },
+          {
+            name: 'url',
+            label: 'URL',
+            formConfig: {
+              rules: [{ required: true, message: '请输入URL', trigger: 'blur' }]
+            }
+          },
+          {
+            name: 'create_time',
+            label: '创建时间',
+            formConfig: {
+              hidden: true
+            },
+            searchConfig: {
+              hidden: true
+            }
+          }
+        ]
+      }
     }
   },
   methods: {
+    handlerTabs(tab, event) {
+      this.size = tab.index === '2' ? '50%' : '25%'
+      this.index = tab.index
+    },
+    hanlderSave() {
+      if (this.index === '0') {
+        this.authorizedMenus()
+      }
+      if (this.index === '1') {
+        this.authorizedOperations()
+      }
+      if (this.index === '2') {
+        this.authorizedResources()
+      }
+    },
     async authorizedDialog() {
       this.drawer = true
       this.keys.menuKey += 1
@@ -65,6 +131,11 @@ export default {
       const resOperations = await operations(this.item.id)
       if (resOperations.data.code === 200) {
         this.operations = resOperations.data.data
+      }
+
+      const resResources = await resources(this.item.id)
+      if (resResources.data.code === 200) {
+        this.resources = resResources.data.data
       }
     },
     async authorizedMenus() {
@@ -86,18 +157,23 @@ export default {
         })
         this.drawer = false
       }
+    },
+    async authorizedResources() {
+      const res = await authorizedResources(this.item.id, { permissions: [...this.resources] })
+      if (res.data.code === 200) {
+        this.$message({
+          message: '授权成功',
+          type: 'success'
+        })
+        this.drawer = false
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-/* .page {
-  padding: 20px;
-} */
 .footer{
-  position: fixed;
-  bottom: 20px;
-  right: 220px;
+  margin-top: 20px;
 }
 </style>
