862fd906be138979e74bf98cb397f1c47b4aabd2
kushu001 <yj_tao123@sina.com>
2023-07-10 22:48:32 +0800

    feat(增加岗位选择组件): 增加岗位选择组件

---
+++ src/views/components/cdp-post-select-table/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <cdp-select-table v-model="postId" :title="title" :columns="columns" :url="url" style="width:200px" />
+</template>
+<script>
+import CdpSelectTable from '@/components/cdp-ui/CdpSelectTable'
+
+export default {
+  components: {
+    CdpSelectTable
+  },
+  props: {
+    value: {
+      type: [String, Number],
+      default: null
+    }
+  },
+  data() {
+    console.log('post-table')
+    return {
+      url: '/api/v1/post',
+      postId: this.value,
+      title: '岗位',
+      columns: [
+        {
+          name: 'name',
+          label: '岗位名称'
+        },
+        {
+          name: 'code',
+          label: '岗位编码'
+        }
+      ]
+    }
+  },
+  watch: {
+    userId(val) {
+      this.$emit('input', val)
+    },
+    value(val) {
+      this.postId = val
+    }
+  }
+}
+</script>
