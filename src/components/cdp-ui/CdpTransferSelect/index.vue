831d30879df0640d641bc5b1f676c488605a8e91
kushu001 <yj_tao123@sina.com>
2023-07-27 10:50:24 +0800

    fix(cdptransferselect): 穿梭组件绑定数据BUG处理

--- src/components/cdp-ui/CdpTransferSelect/index.vue
+++ src/components/cdp-ui/CdpTransferSelect/index.vue
@@ -5,7 +5,7 @@
     </el-input>
     <el-dialog v-el-drag-dialog :append-to-body="true" :visible.sync="visible" width="625px" :title="title">
       <el-transfer
-        v-model="value"
+        v-model="innerValue"
         filterable
         :filter-method="filterMethod"
         filter-placeholder="请输入关键字"
@@ -43,7 +43,8 @@ export default {
   data() {
     return {
       visible: false,
-      inputValue: this.value.length > 0 ? this.value.map(item => this.data.filter(it => it.key === item)[0]).map(item => item.label).join(',') : undefined,
+      inputValue: this._filterDatas(this.value, this.data),
+      innerValue: [],
       filterMethod(query, item) {
         return item.label.indexOf(query) > -1
       }
@@ -51,24 +52,20 @@ export default {
   },
   watch: {
     value(val) {
-      if (!val || val.length <= 0 || this.data.length <= 0 || !val[0]) {
-        this.inputValue = ''
-      } else if (this.data.length > 0) {
-        this.inputValue = this.value.length > 0 ? this.value.map(item => this.data.filter(it => it.key === item)[0]).map(item => item.label).join(',') : undefined
-      }
+      this.inputValue = this._filterDatas(val, this.data)
     },
     data(data) {
-      if (!this.value || this.value.length <= 0 || data.length <= 0 || !this.value[0]) {
-        this.inputValue = ''
-      } else if (this.data.length > 0) {
-        this.inputValue = this.value.length > 0 ? this.value.map(item => this.data.filter(it => it.key === item)[0]).map(item => item.label).join(',') : undefined
-      }
+      this.inputValue = this._filterDatas(this.value, data)
     }
   },
   methods: {
     handleChange(value, direction, movedKeys) {
-      this.inputValue = value.map(item => this.data.filter(it => it.key === item)[0]).map(item => item.label).join(',')
+      this.inputValue = this._filterDatas(value, this.data)
       this.$emit('input', value)
+    },
+    _filterDatas(value, data) {
+      this.innerValue = value
+      return value ? data.filter(it => value.includes(it.key)).map(item => item.label).join(',') : undefined
     }
   }
 }
